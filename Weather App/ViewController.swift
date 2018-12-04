d6a8cabf413ebae2190d6c44e86347d5db05feb8
Bizet Rodriguez <brodriguez-velez@ucmerced.edu>
2018-12-04 13:32:46 -0800

    Initial Commit

---
+++ Weather App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Bizet Rodriguez on 12/4/18.
+//  Copyright © 2018 Bizet Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
